d54387ad3f13c1f119cb4ea56d12f28b7926cbe7
Alena Sharai <exultantislupus@gmail.com>
2025-12-04 19:09:24 +0000

    feat: update detailde article component and article list component

--- frontend/src/components/ArticleList.jsx
+++ frontend/src/components/ArticleList.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+
 import ArticleListItem from './ArticleListItem';
 import mockPosts from '../mockPosts';
 
--- frontend/src/components/ArticleListItem.jsx
+++ frontend/src/components/ArticleListItem.jsx
@@ -1,10 +1,11 @@
+import { Link } from 'react-router-dom';
+
 const ArticleListItem = ({ post }) => {
   return (
     <div>
       <h2>{post.name}</h2>
       <p>{post.text}</p>
-      <p>{post.name}</p>
-      <a href={`/${post.id}`}>Read more</a>
+      <Link to={`/articles/${post.id}`}>Read more</Link>
     </div>
   );
 };
