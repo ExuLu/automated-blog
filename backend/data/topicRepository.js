357d1ea834aa2bde69b58b9a237c01cdb54a0feb
Alena Sharai <exultantislupus@gmail.com>
2025-12-11 13:07:10 +0000

    feat: create topics.json and read globally from it in topic repository

---
+++ backend/data/topicRepository.js
@@ -0,0 +1,15 @@
+let topics = [];
+const FILE_PATH = path.join(__dirname, 'topics.json');
+
+try {
+  topics = JSON.parse(fs.readFileSync(FILE_PATH, 'utf-8'));
+} catch (err) {
+  if (err.code === 'ENOENT') {
+    console.log('topics.json not found. Creating an empty file...');
+    fs.writeFileSync(FILE_PATH, '[]', 'utf-8');
+    topics = [];
+  } else {
+    console.error('Error reading topics.json:', err);
+    topics = [];
+  }
+}
